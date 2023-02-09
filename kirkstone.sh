6a8c31da41b48372b2f5a81f4d977cff98c2ca65
Ulf Samuelsson <ulf@emagii.com>
2023-02-09 22:21:30 +0100

    fix meta-laird-cp COMPAT
    Signed-off-by: Ulf Samuelsson <ulf@emagii.com>

--- kirkstone.sh
+++ kirkstone.sh
@@ -5,7 +5,7 @@ repo init -u  git@github.com:emagii/yocto-emagii-distribution.git -m kirkstone.x
 repo sync --no-clone-bundle
 repo forall -pc 'git checkout --track $REPO_REMOTE/$REPO_RREV'
 
-echo "LAYERSERIES_COMPAT_laird-cp = \"kirkstone\""  >> poky/meta-laird-cp/meta-laird-cp/conf/layer.conf
+echo "LAYERSERIES_COMPAT_summit-radio = \"kirkstone\""  >> poky/meta-laird-cp/meta-summit-radio/conf/layer.conf
 
 chmod 775 oe-init-emagii-sdk
 source ./oe-init-emagii-sdk
